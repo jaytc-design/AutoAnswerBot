72d6e9da2fabee1f2617a2aef01b63e9189e000b
jaytc-design <66380812+jaytc-design@users.noreply.github.com>
2023-05-06 09:48:00 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,37 @@
+(async () => {
+    let n = document.createElement('iframe');
+    document.body.append(n);
+    window.alert = n.contentWindow.alert.bind(window);
+    window.prompt = n.contentWindow.prompt.bind(window);
+    window.confirm = n.contentWindow.confirm.bind(window);
+    n.remove();
+            function reactHandler() {
+                return Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner;
+            };
+            function questionsPromise() {
+                return new Promise((resolve, reject) => {
+                  setInterval(() => {
+                    if (reactHandler().memoizedState.stage == "question") {
+                        setTimeout(resolve,200)
+                    }
+                  }, 0);
+                });
+            }
+
+            if (window.location.pathname != '/play/') {
+                alert('You must be in a gold quest game!');
+            } else {
+                await() questionsPromise();
+                jQuery(".styles__answerContainer___3WS-k-camelCase").each(function() { 
+                  const element = jQuery(this)
+                  const answers = reactHandler().stateNode.state.question.correctAnswers
+                  const innerHTML = element.children().children().children()[0].innerHTML
+
+                  if (answers.includes(innerHTML)) {
+                    element.trigger("click")
+                    return false
+                  }
+                })
+
+            };
+})();
